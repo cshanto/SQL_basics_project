75ff77b3c5062680f28fabc481d19244a6795c03
Chakma S <83701801+cshanto@users.noreply.github.com>
2024-01-01 02:05:50 +0530

    Update Query_file.sql

--- Query_file.sql
+++ Query_file.sql
@@ -1,29 +1,4 @@
--- This is short Library Management System Database
--- Its goal is to implement fundamental MySQL queries
--- Following are the phases of the entire Database System
--- 1. Database Schema [Here: Library]
--- Database Schema is the blueprint of the Database containing the relationships of the tables in it.
--- All tables in the schema {ERD is given}
--- : Books table, Authors table, Users table, and Transactions table
--- 2.Create the mentioned tables with Primary Keys(PM) ans Foreign Keys(FK) for relationships
--- 3. Populate the created tables with appropriate data
--- 4. Perform basic Operations(SQl Queries) :
--- * Adding new books and authors to the system
--- * Registerig new users
--- * Borrowing and returning books
--- * Retrieeving a list of books that are available
--- * Displaying users borrowing history
 
--- 5.Perform complex operations : 
--- * Find the most borrowed book
--- * Identify user with most borrowed book
--- * Calculate late returns and fines
--- 6. Reporting :
--- * Create SQL queries for generating reports on overdue books, ppopular genre, and activity summary
--- 7. Indexing and Optimization :
--- Experiment with indexing on appropriate columns to optimize query performance, especially for large datasets. 
-
--- note : Kindly ignore if any mistakes are found. And I will try to add more stuffs into it 
 
 -- The following statements are covered :
 -- DDL commands such as 'CREATE', 'DROP', 'ALTER'
